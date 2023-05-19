59ca88aab1dea8dea01adccc933ced0701b89b5f
Alausa-AbdulAzeez <alausaabdulazeez@gmail.com>
2023-05-19 21:38:54 +0100

    changed base URL

--- src/components/dashboardCard/DashboardCard.jsx
+++ src/components/dashboardCard/DashboardCard.jsx
@@ -1,29 +1,28 @@
 /* eslint-disable react/prop-types */
-import React from 'react'
+import React from "react";
 
-import { Link } from 'react-router-dom'
-import './dashboardCard.scss'
+import { Link } from "react-router-dom";
+import "./dashboardCard.scss";
 
 const DashboardCard = (props) => {
-  console.log(props)
-  const cardInfo = props.data
+  const cardInfo = props.data;
 
   return (
     <div
-      className='dashboardCardWrapper'
+      className="dashboardCardWrapper"
       style={{ backgroundColor: cardInfo.backgroundColor }}
     >
       <p>{cardInfo.title}</p>
       <h1>{cardInfo.name} </h1>
       {/* {cardInfo.isMoney ? <h1>{props.userName} </h1> : <h1>100+ </h1>} */}
-      <div className='cardBottom'>
-        <Link to={cardInfo.link} className='cardBottomText'>
+      <div className="cardBottom">
+        <Link to={cardInfo.link} className="cardBottomText">
           {cardInfo.linkText}
         </Link>
         {cardInfo.icon}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default DashboardCard
+export default DashboardCard;
